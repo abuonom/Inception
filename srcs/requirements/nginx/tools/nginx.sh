de093ba7ec36a4508b62362cf682f0dc22e387ce
Alessio Buonomo <alessio@Alessio.PC>
2024-04-27 01:10:05 +0200

    ennesima prova

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i -r "s#third#$DOMAIN_NAME#g"    /etc/nginx/nginx.conf
 echo "Aspetto 20 secondi prima di configurare Nginx..."
 sleep 20
 
@@ -8,6 +9,5 @@ chmod 755 /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress/
 
 echo "Modifico il file nginx.conf con il nome del server..."
-sed -i -r "s#third#$DOMAIN_NAME#g"    /etc/nginx/nginx.conf
 echo "Avvio Nginx..."
 nginx -g 'daemon off;'
