4cf339f2f5fe447e058c8375b0f965caa9b99bac
Alessio Buonomo <alessio@Alessio.PC>
2024-04-27 00:31:49 +0200

    added some

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -7,5 +7,8 @@ echo "Imposto i permessi per /var/www/wordpress..."
 chmod 755 /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress/
 
+echo "Modifico il file nginx.conf con il nome del server..."
+sed -i "s/NOMESERVER/${USER}.42.fr/g" /etc/nginx/nginx.conf
+
 echo "Avvio Nginx..."
 nginx -g 'daemon off;'
