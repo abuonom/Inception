6fc4df62129b754ca835bba9d729272052d201f1
Alessio Buonomo <alessio@Alessio.PC>
2024-04-27 01:04:25 +0200

    Sto cercando di sistemare il problema che ho con il redirect di localhost su login.42.fr

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -8,6 +8,6 @@ chmod 755 /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress/
 
 echo "Modifico il file nginx.conf con il nome del server..."
-sed -i "s/NOMESERVER/$USER.42.fr/g" /etc/nginx/nginx.conf
+sed -i -r "s#third#$DOMAIN_NAME#g"    /etc/nginx/nginx.conf
 echo "Avvio Nginx..."
 nginx -g 'daemon off;'
